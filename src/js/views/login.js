87a9273ba34f90116a3fce91636cacaf3210f085
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-12 12:55:35 -0500

    adjusting styling

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -7,39 +7,55 @@ export const Login = () => {
 	return (
 		<div className="backColor">
 			<div className="side1">
-				<div className="about" />
-				<img className="imgLogo" src={logo} alt="..." />
-				<h1 className="text-center mt-2">Mission Statement</h1>
-				<p className="text-center mt-2">
-					Our passion is to provide the top possible matches between employer and contractor in any
-					industries. We have a foundational belief that creating relationships and connecting people is what
-					moves companies and employees forward.
-				</p>
-			</div>
-
-			<form className="p-3 login">
-				<h1 className="text-center mt-2">Login</h1>
+				{/* Image Logo */}
 				<div>
-					<div className="form-group">
-						<label>E-mail</label>
-						<input type="text" className="form-control" placeholder="Enter Your Email" />
-					</div>
-					<div className="form-group">
-						<label>Password</label>
-						<input type="text" className="form-control" placeholder="Enter Your Password" />
-					</div>
+					<img className="imgLog" src={logo} alt="..." />
 				</div>
-				<Link to={"/signup"}>
-					<button style={{ width: "100px" }} type="button" className="btn btn-primary form-control mr-5">
-						Sign Up
-					</button>
-				</Link>
-				<Link to="/">
-					<button className="btn btn-primary form-control" style={{ width: "100px" }}>
-						Login
-					</button>
-				</Link>
-			</form>
+				{/* Mission Statement */}
+				<div className="about">
+					<h4 className="text-center mb-0 mt-2 missionSta">Mission Statement</h4>
+					<blockquote className="mb-1">
+						<p className="justify-content mb-1 pr-3 pl-3 mt-2">
+							&quot;Our passion is to provide the top possible matches between employer and contractor in
+							any industries. We have a foundational belief that creating relationships and connecting
+							people is what moves companies and employees forward.&quot;
+						</p>
+						{/* <p className="font-italic">Santiago Gonçalo</p> */}
+						<cite className="float-right mr-2" title="Santiago Gonçalo">
+							Santiago Gonçalo
+						</cite>
+					</blockquote>
+				</div>
+				{/* Login Component */}
+				<form className="p-3 login">
+					<h1 className="text-center mt-2">Login</h1>
+					<div>
+						<div className="form-group">
+							<label>E-mail</label>
+							<input type="text" className="form-control" placeholder="Enter Your Email" />
+						</div>
+						<div className="form-group">
+							<label>Password</label>
+							<input type="text" className="form-control" placeholder="Enter Your Password" />
+						</div>
+					</div>
+					<div className="buttonLogin">
+						<Link to={"/signup"}>
+							<button
+								style={{ width: "100px" }}
+								type="button"
+								className="btn btn-primary form-control mr-5">
+								Sign Up
+							</button>
+						</Link>
+						<Link to="/">
+							<button className="btn btn-primary form-control" style={{ width: "100px" }}>
+								Login
+							</button>
+						</Link>
+					</div>
+				</form>
+			</div>
 			<div className="side2">
 				<img src={street} className="imgLogin" alt="..." />
 			</div>
