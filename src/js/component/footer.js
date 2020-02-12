f23df3380fbf60d188ef63dcc098670a6e3ca8bb
João Henrique Xavier <jhcxavier@gmail.com>
2020-02-12 17:41:04 -0500

    updates

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto pt-4 text-center">
 		<p>
 			<i className="far fa-copyright" />
 			Task Rider
